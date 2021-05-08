861004a3640779b35e1623d3305f42e5bd14a863
ldg <ldg@LDG.LDG>
2021-05-08 06:28:03 +0100

    little added. Need to figure out how to add line numbers

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -3,12 +3,13 @@ use nom::{self, IResult};
 use super::ast::*;
 
 /* A parser can have any output type T. It has a &str for input and for output
- * message*/
-type ParseResult<T> = IResult<&str, T, &str>; 
+ * message. The error also contains a line number */
+type ParseResult<T> = IResult<&str, T, (&str, u32)>; 
 
 impl Ast {
     
-    fn reg(input: &str) {
+    fn reg(input: &str) -> ParseResult<Reg> {
         
     }
+
 }
